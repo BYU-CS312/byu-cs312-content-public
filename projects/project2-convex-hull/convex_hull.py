e98c9c6b94f6cdcf5b3bb4569be818fae8f05175
Hunter Larson <larso38@byu.edu>
2023-09-19 15:15:04 -0600

    copy from source repo

--- projects/project2-convex-hull/convex_hull.py
+++ projects/project2-convex-hull/convex_hull.py
@@ -1,75 +1,80 @@
-# this is 4-5 seconds slower on 1000000 points than Ryan's desktop...  Why?
-
-
-from PyQt5.QtCore import QLineF, QPointF, QThread, pyqtSignal
+from which_pyqt import PYQT_VER
+if PYQT_VER == 'PYQT5':
+	from PyQt5.QtCore import QLineF, QPointF, QObject
+elif PYQT_VER == 'PYQT4':
+	from PyQt4.QtCore import QLineF, QPointF, QObject
+elif PYQT_VER == 'PYQT6':
+	from PyQt6.QtCore import QLineF, QPointF, QObject
+else:
+	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
 
 import time
 
+# Some global color constants that might be useful
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+# Global variable that controls the speed of the recursion automation, in seconds
+PAUSE = 0.25
+
+#
+# This is the class you have to complete.
+#
+class ConvexHullSolver(QObject):
+
+# Class constructor
+	def __init__( self):
+		super().__init__()
+		self.pause = False
+
+# Some helper methods that make calls to the GUI, allowing us to send updates
+# to be displayed.
+
+	def showTangent(self, line, color):
+		self.view.addLines(line,color)
+		if self.pause:
+			time.sleep(PAUSE)
+
+	def eraseTangent(self, line):
+		self.view.clearLines(line)
+
+	def blinkTangent(self,line,color):
+		self.showTangent(line,color)
+		self.eraseTangent(line)
+
+	def showHull(self, polygon, color):
+		self.view.addLines(polygon,color)
+		if self.pause:
+			time.sleep(PAUSE)
+
+	def eraseHull(self,polygon):
+		self.view.clearLines(polygon)
+
+	def showText(self,text):
+		self.view.displayStatusText(text)
 
 
-class ConvexHullSolverThread(QThread):
-    def __init__( self, unsorted_points,demo):
-        self.points = unsorted_points                    
-        self.pause = demo
-        QThread.__init__(self)
+# This is the method that gets called by the GUI and actually executes
+# the finding of the hull
+	def compute_hull( self, points, pause, view):
+		self.pause = pause
+		self.view = view
+		assert( type(points) == list and type(points[0]) == QPointF )
 
-    def __del__(self):
-        self.wait()
+		t1 = time.time()
+		# TODO: SORT THE POINTS BY INCREASING X-VALUE
+		t2 = time.time()
 
-    # These two signals are used for interacting with the GUI.
-    show_hull    = pyqtSignal(list,tuple)
-    display_text = pyqtSignal(str)
+		t3 = time.time()
+		# this is a dummy polygon of the first 3 unsorted points
+		polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
+		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
+		t4 = time.time()
 
-    # Some additional thread signals you can implement and use for debugging,
-    # if you like
-    show_tangent = pyqtSignal(list,tuple)
-    erase_hull = pyqtSignal(list)
-    erase_tangent = pyqtSignal(list)
-                    
-
-    def set_points( self, unsorted_points, demo):
-        self.points = unsorted_points
-        self.demo   = demo
-
-
-    def run(self):
-        assert( type(self.points) == list and type(self.points[0]) == QPointF )
-
-        n = len(self.points)
-        print( 'Computing Hull for set of {} points'.format(n) )
-
-        t1 = time.time()
-        # TODO: SORT THE POINTS BY INCREASING X-VALUE
-        t2 = time.time()
-        print('Time Elapsed (Sorting): {:3.3f} sec'.format(t2-t1))
-
-        t3 = time.time()
-        # TODO: COMPUTE THE CONVEX HULL USING DIVIDE AND CONQUER
-        t4 = time.time()
-
-        USE_DUMMY = True
-        if USE_DUMMY:
-            # This is a dummy polygon of the first 3 unsorted points
-            polygon = [QLineF(self.points[i],self.points[(i+1)%3]) for i in range(3)]
-            
-            # When passing lines to the display, pass a list of QLineF objects.
-            # Each QLineF object can be created with two QPointF objects
-            # corresponding to the endpoints
-            assert( type(polygon) == list and type(polygon[0]) == QLineF )
-
-            # Send a signal to the GUI thread with the hull and its color
-            self.show_hull.emit(polygon,(0,255,0))
-
-        else:
-            # TODO: PASS THE CONVEX HULL LINES BACK TO THE GUI FOR DISPLAY
-            pass
-
-            
-        # Send a signal to the GUI thread with the time used to compute the 
-        # hull
-        self.display_text.emit('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
-        print('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
-            
-
+		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
+		# object can be created with two QPointF objects corresponding to the endpoints
+		self.showHull(polygon,RED)
+		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
--- projects/project4-gene-sequencing/GeneSequencing.py
+++ projects/project4-gene-sequencing/GeneSequencing.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-#from PyQt5.QtCore import QLineF, QPointF
+from which_pyqt import PYQT_VER
+if PYQT_VER == 'PYQT5':
+	from PyQt5.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT4':
+	from PyQt4.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT6':
+	from PyQt6.QtCore import QLineF, QPointF
+else:
+	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
-
-
-import math
-import time
+import random
 
 # Used to compute the bandwidth for banded version
 MAXINDELS = 3
@@ -17,37 +22,24 @@ SUB = 1
 
 class GeneSequencing:
 
-    def __init__( self ):
-        pass
+	def __init__( self ):
+		pass
 
-# This is the method called by the GUI.  _sequences_ is a list of the ten sequences, _table_ is a
-# handle to the GUI so it can be updated as you find results, _banded_ is a boolean that tells
-# you whether you should compute a banded alignment or full alignment, and _align_length_ tells you 
+# This is the method called by the GUI.  _seq1_ and _seq2_ are two sequences to be aligned, _banded_ is a boolean that tells
+# you whether you should compute a banded alignment or full alignment, and _align_length_ tells you
 # how many base pairs to use in computing the alignment
-    def align( self, sequences, table, banded, align_length ):
-        self.banded = banded
-        self.MaxCharactersToAlign = align_length
-        results = []
 
-        for i in range(len(sequences)):
-            jresults = []
-            for j in range(len(sequences)):
-                if j < i:
-                   s = {}
-                else:
+	def align( self, seq1, seq2, banded, align_length):
+		self.banded = banded
+		self.MaxCharactersToAlign = align_length
+
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
-                    score = i + j
-                    alignment1 = 'abc-easy  DEBUG:(seq{}, {} chars,align_len={}{})'.format(i+1,
-                        len(sequences[i]), align_length, ',BANDED' if banded else '')
-                    alignment2 = 'as-123--  DEBUG:(seq{}, {} chars,align_len={}{})'.format(j+1,
-                        len(sequences[j]), align_length, ',BANDED' if banded else '')
-###################################################################################################                    
-                    s = {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
-                    table.item(i,j).setText('{}'.format(int(score) if score != math.inf else score))
-                    table.repaint()    
-                jresults.append(s)
-            results.append(jresults)
-        return results
+		score = random.random()*100;
+		alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
+			len(seq1), align_length, ',BANDED' if banded else '')
+		alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
+			len(seq2), align_length, ',BANDED' if banded else '')
+###################################################################################################
 
-
+		return {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
