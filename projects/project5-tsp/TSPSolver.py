552838c09ec22270e3b3511d627309a4236db450
Haley Nelson <113866972+hmnelson13@users.noreply.github.com>
2023-12-03 16:59:42 -0700

    Remove references to group project in readme

--- projects/project5-tsp/TSPSolver.py
+++ projects/project5-tsp/TSPSolver.py
@@ -69,9 +69,7 @@ class TSPSolver:
 
 
 	''' <summary>
-		This is the entry point for the greedy solver, which you must implement for
-		the group project (but it is probably a good idea to just do it for the branch-and
-		bound project as a way to get your feet wet).  Note this could be used to find your
+		This is the entry point for the greedy solver.  Note this could be used to find your
 		initial BSSF.
 		</summary>
 		<returns>results dictionary for GUI that contains three ints: cost of best solution,
@@ -95,18 +93,4 @@ class TSPSolver:
 	'''
 
 	def branchAndBound( self, time_allowance=60.0 ):
-		pass
-
-
-
-	''' <summary>
-		This is the entry point for the algorithm you'll write for your group project.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution,
-		time spent to find best solution, total number of solutions found during search, the
-		best solution found.  You may use the other three field however you like.
-		algorithm</returns>
-	'''
-
-	def fancy( self,time_allowance=60.0 ):
 		pass
